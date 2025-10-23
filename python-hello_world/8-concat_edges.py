d4e507329659454dfdf3be3b124af31ee6530600
Maga01A <alnatluxanov@gmail.com>
2025-10-23 09:54:30 +0000

    Code

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# slicing ile istediğimiz kısmı yazdır
-print(str[52:74] + str[:6])  # "object-oriented programming with Python"
+# slicing ile istenen kısmı yazdır
+print(str[52:74] + str[0:6])
