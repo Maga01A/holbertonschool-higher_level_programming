e876e76866fd32da331888e82de2f2287748ef77
Maga01A <alnatluxanov@gmail.com>
2025-10-25 15:47:09 +0000

    import task

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    argv = sys.argv
+    count = len(argv) - 1
+    if count = 0:
+        print("0 argument(s).")
+    else:
+        print("{} argument(s):".format(count))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
