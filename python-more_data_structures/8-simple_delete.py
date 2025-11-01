ae6ce41a3d7c04fde27ee11aaf95bb07fb470de0
Maga2003 <atluxanov2002@gmail.com>
2025-11-01 17:55:58 +0400

    task

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    """Deletes a key from a dictionary if it exists.
-    Args:
-        a_dictionary (dict): The dictionary to delete the key from.
-        key: The key to delete.
-        
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
