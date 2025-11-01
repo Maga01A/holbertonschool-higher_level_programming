a8e352315d629e5a5d13d0fe3b1eb1bce3a9afbd
Maga01A <alnatluxanov@gmail.com>
2025-11-01 14:43:53 +0000

    all

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""Module that defines a function to add two integers."""
+"""Modul for adding two integers"""
+
 
 def add_integer(a, b=98):
-    """Add two integers and return the result.
+    """This function adds two integer"""
 
-    a and b must be integers or floats, otherwise a TypeError is raised.
-    Floats are casted to integers before addition.
-    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    return int(a+b)
--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,41 +1,24 @@
 #!/usr/bin/python3
-"""Module that divides all elements of a matrix."""
+"""Modul for divides all elements of a matrix"""
 
 
 def matrix_divided(matrix, div):
-    """Divides all elements of a matrix by div.
-
-    Args:
-        matrix (list of lists of int/float): The matrix to divide.
-        div (int/float): The divisor.
-
-    Returns:
-        list: A new matrix with all elements divided by div and rounded to 2 decimals.
-
-    Raises:
-        TypeError: If matrix is not a list of lists of numbers,
-                   or rows are not of the same size,
-                   or div is not a number.
-        ZeroDivisionError: If div is 0.
-    """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats"
-        )
-
-    if len(matrix) == 0 or any(len(row) != len(matrix[0]) for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-
-    for row in matrix:
-        if not all(isinstance(x, (int, float)) for x in row):
-            raise TypeError(
-                "matrix must be a matrix (list of lists) of integers/floats"
-            )
-
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
+    """This function divides all elements of a matrix"""
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
-    return [[round(x / div, 2) for x in row] for row in matrix]
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if not all(len(matrix[0]) == len(matrix[i]) for i in range(len(matrix))):
+        raise TypeError("Each row of the matrix must have the same size")
+    new = []
+    a = "matrix must be a matrix (list of lists) of integers/floats"
+    for row in matrix:
+        new_row = []
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError(a)
+            element = round(float(element) / div, 2)
+            new_row.append(element)
+        new.append(new_row)
+    return new
