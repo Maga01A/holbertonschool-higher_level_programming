db62def87676ff05275dc5d476858644c599add8
Maga2003 <atluxanov2002@gmail.com>
2025-11-01 17:28:25 +0400

    task

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!usr/bin/python3
+def divisible_by_2(my_list=[]):
+    """Find all multiples of 2 in a list.
+
+    Args:
+        my_list (list): The list of integers.
+
+    Returns:
+        list: A new list with True or False, depending on whether the
+        integer at the same position in the original list is a multiple of 2.
+    """
+    return [i % 2 == 0 for i in my_list]      
