1fa63e054dd66c2a132458b1d1535ec8ee054465
Maga2003 <atluxanov2002@gmail.com>
2025-11-07 16:01:40 +0400

    task

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""Square class that defines a square by its size."""
+
 class Square:
-    pass
+    """Class that defines a square."""
+
+    def __init__(self, size):
+        """Initialize the square with a given size."""
+        self.__size = size
