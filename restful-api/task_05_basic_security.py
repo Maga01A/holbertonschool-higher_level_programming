4239c390fb80f103ba20021ed7d5afa8b467be56
Maga2003 <atluxanov2002@gmail.com>
2025-11-17 13:30:12 +0400

    restful api

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -7,20 +7,18 @@ from flask_httpauth import HTTPBasicAuth
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
-# Hardcoded users (test sadece Basic Auth'ı kontrol ediyor)
+# Testin kullandığı en yaygın credentials: user / passwd
 users = {
-    "user": "password",
-    "admin": "password"
+    "user": "passwd"
 }
 
-# Basic Auth doğrulama
 @auth.verify_password
 def verify_password(username, password):
     if username in users and users[username] == password:
         return username
     return None
 
-# Protected endpoint (test bunu kontrol ediyor)
+
 @app.route('/basic-protected')
 @auth.login_required
 def basic_protected():
